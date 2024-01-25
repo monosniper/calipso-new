2e9091638906dfc0d87abdd0c43bb3a227666ba7
monosniper <monosniper@gmail.com>
2024-01-25 22:52:45 +0500

    asdad

--- database/seeders/FreelancersSeeder.php
+++ database/seeders/FreelancersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 
@@ -17,11 +18,14 @@ class FreelancersSeeder extends Seeder
     public function run()
     {
         $role = Role::where('name', 'freelancer')->first();
-        $avatars = Storage::files('avatars');
-        foreach (User::latest()->limit(300)->get() as $user) {
+//        $avatars = Storage::files('avatars');
+        foreach (User::latest()->get() as $user) {
+//        foreach (User::latest()->limit(300)->get() as $user) {
 //            $user->roles()->save($role);
+            $user->created_at = Carbon::today()->subDays(rand(0, 365 * 3));
+            $user->save();
 
-            $user->addMediaFromDisk($avatars[array_rand($avatars)], 'local')->preservingOriginal()->toMediaCollection('avatar');
+//            $user->addMediaFromDisk($avatars[array_rand($avatars)], 'local')->preservingOriginal()->toMediaCollection('avatar');
 
 //            $user->delete();
         }
