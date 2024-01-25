1eeec6f65004970f0904c2a55b2add4e6cb0f47e
monosniper <monosniper@gmail.com>
2024-01-26 00:03:42 +0500

    asdad

--- resources/views/freelance/freelancer/reviews.blade.php
+++ resources/views/freelance/freelancer/reviews.blade.php
@@ -10,11 +10,12 @@
     <div class="grey_block">
         <div class="profile_title profile_title_header">
             <span>{{ __('dashboard.reviews_for', ['name' => $user->fullName]) }} <small>({{$reviews->count()}})</small></span>
-            @auth
-                @if(auth()->user()->can('create', [App\Models\Review::class, $user->id]))
-                    <button class="webz_btn white bordered open_modal" modal-wrapper="#add-review">{{__('main.make_review')}}</button>
-                @endif
-            @endauth
+{{--            @auth--}}
+{{--                @if(auth()->user()->can('create', [App\Models\Review::class, $user->id]))--}}
+{{--                    <button class="webz_btn white bordered open_modal" modal-wrapper="#add-review">{{__('main.make_review')}}</button>--}}
+{{--                @endif--}}
+{{--            @endauth--}}
+            <button class="webz_btn white bordered open_modal" modal-wrapper="#add-review">{{__('main.make_review')}}</button>
         </div>
 
         <div class="portfolios_tags">
