6d9c3b4e64027265d2ed731b713155937836222a
monosniper <monosniper@gmail.com>
2024-01-26 15:58:20 +0500

    asdad

--- resources/views/includes/modals/portfolio.blade.php
+++ resources/views/includes/modals/portfolio.blade.php
@@ -11,7 +11,7 @@
             <ul class="slider ">
                 @foreach($portfolio->getMedia('preview') as $image)
                     <li>
-                        <input type="radio" id="slide{{ $loop->index }}" name="slide">
+                        <input type="radio" id="slide{{ $loop->index }}" name="slide" checked="checked">
                         <label for="slide{{ $loop->index }}"></label>
                         <img src="{{ $image->getFullUrl() }}" alt="{{ $portfolio->title }}">
                     </li>
