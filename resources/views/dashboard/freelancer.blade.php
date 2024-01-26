3081d11f309e858a037631aaa5caedba17a03cfb
monosniper <monosniper@gmail.com>
2024-01-26 15:32:07 +0500

    asdad

--- resources/views/dashboard/freelancer.blade.php
+++ resources/views/dashboard/freelancer.blade.php
@@ -8,7 +8,7 @@
     <div class="grey_block">
         <div class="profile_title">{{__('main.become_freelancer')}}</div>
         <div class="profile__description">
-            <div>{{__('dashboard.become_text')}}</div>
+            <div>{!! __('dashboard.become_text') !!}</div>
         </div>
         <div class="profile__footer">
             <a href="{{ route('forms.become') }}" class="webz_btn">{{ __('main.become_freelancer') }}</a>
